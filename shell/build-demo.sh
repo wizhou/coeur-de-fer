0ebe4469094065ef647202c8d07528a891d73f9e
wizhou <coucou@romaingoetz.fr>
2022-01-07 16:33:59 +0100

    Workflow : Build demo script
    Add a script to make parcel build the demo site css

---
+++ shell/build-demo.sh
@@ -0,0 +1,33 @@
+echo "----------------------------"
+echo "🏹 Deleting old dist folder. "
+echo "----------------------------"
+
+# Look for files in a dist folder and remove them.
+# Change the folder location if needed.
+
+DISTFOLDER='./demo/dist/*'
+
+if rm -rf $DISTFOLDER
+then
+  echo "✅ Old dist folder has been removed"
+fi
+
+echo "----------------------------"
+echo "🏹 Deleting Parcel Cache. "
+echo "----------------------------"
+
+# Look for files in a dist folder and remove them.
+# Change the folder location if needed.
+
+CACHEFOLDER='.parcel-cache'
+
+if rm -rf $CACHEFOLDER
+then
+  echo "✅ Old Cache folder has been removed"
+fi
+
+echo "----------------------"
+echo "⚙️: Build demo"
+echo "----------------------"
+
+parcel build './demo/_less/index.less' --dist-dir './demo/dist'
